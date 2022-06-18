bbfbaead18fc3aef168f1d6be5cb9337f2d1506a
Nahidul Islam <nahidkishore99@gmail.com>
2022-06-19 02:02:06 +0600

    product colors section done

--- frontend/src/screens/dashboard/CreateProduct.js
+++ frontend/src/screens/dashboard/CreateProduct.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Wrapper from './Wrapper';
 import ScreenHeader from '../../components/ScreenHeader';
 import { useAllCategoriesQuery } from '../../store/services/categoryService';
 import Spinner from '../../components/Spinner';
+import { TwitterPicker } from 'react-color';
+import { v4 as uuidv4 } from 'uuid';
+import Colors from '../../components/Colors';
+
 const CreateProduct = () => {
   const { data = [], isFetching } = useAllCategoriesQuery();
   console.log(data, isFetching);
+  const [state, setState] = useState({
+    title: '',
+    price: 0,
+    discount: 0,
+    stock: 0,
+    category: '',
+    colors: [],
+  });
+  const handleInput = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  const saveColors = (color) => {
+    console.log(color);
+    const filtered = state.colors.filter((clr) => clr.color !== color.hex);
+    setState({
+      ...state,
+      colors: [...filtered, { color: color.hex, id: uuidv4() }],
+    });
+  };
+ // console.log(state.colors);
+  const deleteColor = (color) => {
+    const filtered = state.colors.filter(clr => clr.color !== color.color);
+    setState({...state, colors: filtered});
+}
   return (
     <Wrapper>
       <ScreenHeader>
@@ -27,6 +55,8 @@ const CreateProduct = () => {
                 className='form-control'
                 id='title'
                 placeholder='title...'
+                onChange={handleInput}
+                value={state.title}
               />
             </div>
             <div className='w-full md:w-6/12 p-3'>
@@ -39,6 +69,8 @@ const CreateProduct = () => {
                 className='form-control'
                 id='price'
                 placeholder='price...'
+                onChange={handleInput}
+                value={state.price}
               />
             </div>
             <div className='w-full md:w-6/12 p-3'>
@@ -51,6 +83,8 @@ const CreateProduct = () => {
                 className='form-control'
                 id='discount'
                 placeholder='discount...'
+                onChange={handleInput}
+                value={state.discount}
               />
             </div>
             <div className='w-full md:w-6/12 p-3'>
@@ -63,6 +97,8 @@ const CreateProduct = () => {
                 className='form-control'
                 id='stock'
                 placeholder='stock...'
+                onChange={handleInput}
+                value={state.stock}
               />
             </div>
             <div className='w-full md:w-6/12 p-3'>
@@ -71,7 +107,13 @@ const CreateProduct = () => {
               </label>
               {!isFetching ? (
                 data?.categories?.length > 0 && (
-                  <select name='categories' id='categories' className='form-control'>
+                  <select
+                    name='category'
+                    id='categories'
+                    className='form-control'
+                    onChange={handleInput}
+                    value={state.category}
+                  >
                     <option value=''>Choose Category</option>
                     {data?.categories?.map((category) => (
                       <option value={category.name} key={category._id}>
@@ -84,9 +126,19 @@ const CreateProduct = () => {
                 <Spinner />
               )}
             </div>
+            <div className='w-full md:w-6/12 p-3'>
+              <label htmlFor='colors' className='label'>
+                Choose colors
+              </label>
+              <label htmlFor=''>
+                <TwitterPicker onChangeComplete={saveColors} />
+              </label>
+            </div>
           </div>
         </div>
-        <div className='w-full xl:w-4/12 p-3'>colors and images</div>
+        <div className='w-full xl:w-4/12 p-3'>
+          <Colors colors={state.colors} deleteColor={ deleteColor}/>
+        </div>
       </div>
     </Wrapper>
   );
