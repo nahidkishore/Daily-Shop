fdac55892317943d9523d1d6839151c014185b94
Nahidul Islam <nahidkishore99@gmail.com>
2022-06-26 23:53:40 +0600

    product save or store in database

--- backend/controllers/Product.js
+++ backend/controllers/Product.js
@@ -2,12 +2,13 @@ const formidable = require('formidable');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const ProductModel = require('../models/ProductModel');
 
 class Product {
   async create(req, res) {
     const form = formidable({ multiples: true });
 
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
       if (!err) {
         //console.log('fields', fields);
         // console.log('files', files);
@@ -41,6 +42,7 @@ class Product {
             errors.push({ image3: 'Image3 is required' });
           }
           if (errors.length === 0) {
+            const images = {};
             for (let i = 0; i < Object.keys(files).length; i++) {
               const mimetype = files[`image${i + 1}`].mimetype;
               const extension = mimetype.split('/')[1].toLowerCase();
@@ -53,9 +55,10 @@ class Product {
                 const __dirname = path.resolve();
                 const newPath =
                   __dirname + `/../frontend/public/images/${imageName}`;
+                images[`image${i + 1}`] = imageName;
                 fs.copyFile(files[`image${i + 1}`].filepath, newPath, (err) => {
-                  if (!err) {
-                    console.log('image uploaded successfully');
+                  if (err) {
+                    console.log(err);
                   }
                 });
               } else {
@@ -68,7 +71,27 @@ class Product {
               console.log(mimetype);
               console.log(extension);
             }
-            if (errors.length !== 0) {
+            if (errors.length === 0) {
+              try {
+                const response = await ProductModel.create({
+                  title: parseData.title,
+                  price: parseInt(parseData.price),
+                  discount: parseInt(parseData.discount),
+                  stock: parseInt(parseData.stock),
+                  category: parseData.category,
+                  colors: parseData.colors,
+                  sizes: JSON.parse(fields.sizes),
+                  image1: images['image1'],
+                  image2: images['image2'],
+                  image3: images['image3'],
+                  description: fields.description,
+                });
+                return res.status(201).json({msg: 'Product has created successfully', response})
+              } catch (error) {
+                console.log(error)
+                return res.status(500).json(error);
+              }
+            } else {
               return res.status(400).json({ errors });
             }
           } else {
